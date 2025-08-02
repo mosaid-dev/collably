044ee1b78f3d0b68a984653ae142a600999758b9
Mohamed Saidi <said53mohamed@gmail.com>
2025-08-02 22:12:30 +0300

    feat: Add initial Javascript file and practices variable

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello from script.js!");
+const name = "mohamed Saidi";
+const age = 23;
+const isStudent = true;
+console.log("name");
+console.log("age");
+console.log("isStudent");
+    
+//console.log(`name: ${name}, age: ${age}, isStudent: ${isStudent}`);
+let info = `My name is ${name} and I am ${age} years old.`;
+console.log(info);
+
+const favNum1 = 5;
+const favNum2 = 10;
+const sum = favNum1 + favNum2;
+console.log(`The sum of ${favNum1} and ${favNum2} is ${sum }.`);
+
+const product = favNum1 * favNum2;
+console.log(`The product of ${favNum1} and ${favNum2} is ${product}.`);
